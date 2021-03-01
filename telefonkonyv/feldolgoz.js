9306b0096d7b530b0c0d5a8a0ccb87e6af7940d7
Komonyi <komonyi1996@gmail.com>
2021-03-01 11:16:58 +0100

    POST/GET, hibás

---
+++ telefonkonyv/feldolgoz.js
@@ -0,0 +1,55 @@
+$(function () {
+
+    $("#beolvas").on("click", beolvas);
+    $("#kuld").on("click", adBeir);
+});
+var telefonkonyvem = [];
+function kiir() {
+//    var nev = $("#nev").val();
+//    var tel = $("#tel").val();
+//    var kep = $("#kep").val();
+    $("article").empty();
+    for (var i = 0; i < telefonkonyvem.length; i++) {
+        var nev = telefonkonyvem[i].nev;
+        var tel = telefonkonyvem[i].tel;
+        var kep = telefonkonyvem[i].kep;
+        var elem = "<div><h2>" + nev + "</h2><p>" + tel + "</p> <p>" + kep + "</p><button class='torol'>Töröl</button></div>";
+        $("article").append(elem);
+    }
+
+
+
+}
+
+function adBeir() {
+//    var nev = $("#nev").val();
+//    var tel = $("#tel").val();
+//    var kep = $("#kep").val();
+    var szemely = {
+
+        nev: $("#nev").val(),
+        tel: $("#tel").val(),
+        kep: $("#kep").val()
+
+    };
+
+
+
+
+
+    $.ajax({
+        type: "POST",
+        url: "beir.php",
+        data: szemely,
+        success: function (ujszemely) {
+
+            console.log(ujszemely);
+            telefonkonyvem = JSON.parse(ujszemely);
+            console.log(telefonkonyvem);
+            kiir();
+        },
+        error: function () {
+            alert("Hiba az adatok mentésekor");
+        }
+    });
+}
